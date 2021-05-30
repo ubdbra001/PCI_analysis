f42c04edeb71ff8ee345582ec414bf55c9557e4e
ubdbra001 <dan.brady@rocketmail.com>
2021-05-30 18:34:47 +0100

    correct dataframe reference in merge_events

--- R/PCI_functions.R
+++ R/PCI_functions.R
@@ -13,7 +13,7 @@ merge_events <- function(events_df, frame_gap, behav_name) {
   # Loops backwards through data frame. Checks gap, if less than frame.gap then
   # update preceding row with current row end points and then delete current row
 
-  if (nrow(events) < 2) return(events_df)
+  if (nrow(events_df) < 2) return(events_df)
 
   # Make sure df is not grouped before processing further
   if (is_grouped_df(events_df)) events_df <- ungroup(events_df)
