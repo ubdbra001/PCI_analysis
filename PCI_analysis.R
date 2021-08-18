28e1a8c577190965286b878303f48389fe70aa63
Dan Brady <daniel.brady@sheffield.ac.uk>
2021-08-18 10:22:10 +0100

    Added analysis steps for removing overlapping events, processing mutual looks, and processing overlaps with naming events.

--- PCI_analysis.R
+++ PCI_analysis.R
@@ -11,7 +11,11 @@ if (remove_ambiguous) {
   output_file <- sprintf("%s_inAmbg.csv", output_file_name)
 }
 
-
+# Prepare empty dataframes for data output
+summary_stats_output <- tibble()
+mutual_looks_output <- tibble()
+overlapping_events_output <- tibble()
+count_naming_overlaps_output <- tibble()
 
 
 # Body of Script ----
@@ -37,5 +41,69 @@ for (file_name in files) {
                           .f = parse_behav_events) %>%
     mutate(event_ID = row_number(), .before = 1)
 
+  # Save raw events
+
+  # Remove events that overlap with offCameras and extend offCameras
+  behav_events <- remove_overlapping_events(
+    target_ev_name = "offCamera",
+    comparator_ev_name = "baby|parent",
+    behav_df_in = behav_events,
+    extend = TRUE)
+
+  # Generate stats for all events
+  event_summary_stats <- summarise_events(behav_events)
+
+  summary_stats_output <- add_summary_stats(event_summary_stats,
+                                            summary_stats_output,
+                                            PartID)
+
+  # Get individual mututl look events
+  mutual_look_events <- process_mutual_looks(behav_events,
+                                               "parentATbaby",
+                                               "babyATparent") %>%
+    add_column(Part_ID = PartID, .before = 1)
+
+  # Bind events for output
+  mutual_looks_output <- bind_rows(mutual_looks_output, mutual_look_events)
+
+  # Still need to deide what to do with summarising events, might want to set up
+  # a seperate function that calls the summary_function on the mutual looks and
+  # the different frist look types
+  # summarise_events(mututal_looks_summary)
+
+  overlapping_events <- map_df(comp_events, find_naming_overlap,
+                               target_event = target_event, data_in = behav_events)
+
+  # Save overlapping events
+  overlapping_events_output <- add_column(overlapping_events, Part_ID = PartID, .before = 1) %>%
+    bind_rows(overlapping_events_output, .)
+
+
+  # Get all of the naming events
+  naming_events <- select_behav(behav_events, target_event) %>%
+    select(event_ID, ordinal, label, referent1, referent2)
+
+  overlapping_ev_counts <- count_naming_overlaps(data_in = overlapping_events,
+                                                 naming_events = naming_events) %>%
+    add_column(Part_ID = PartID, .before = 1)
+
+  count_naming_overlaps_output <- bind_rows(count_naming_overlaps_output, overlapping_ev_counts)
+
+
+
+  # Generate stats for other crossover events
+  #   parentnoun and parent/babyobj - Does the noun refer to something that the parent or baby is holding?
+  #   parentnoun and baby/parentATobj - Does the noun refer to something that the parent or baby is looking at?
+  #
+  #   Will need a function to extend the parentnoun behav and add the noun referent
+
+  # repeat these actions after removing bookreading crossover events
+
 }
+
+save(mutual_looks_output, file = "data/looks_data/mutual_looks.RData")
+save(overlapping_events_output, file = "data/naming_data/overlaping_naming_events.RData")
+
+write_csv(summary_stats_output, "data/event_data/events_summary.csv")
+write_csv(count_naming_overlaps_output, "data/naming_data/overlaps_count.csv")
 
