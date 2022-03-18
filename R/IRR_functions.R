de758e4036c53b666d92edb9f899b215ce1a0f7f
ubdbra001 <dan.t.brady21@gmail.com>
2022-03-18 16:56:36 +0000

    IRR functions

--- R/IRR_functions.R
+++ R/IRR_functions.R
@@ -5,8 +5,58 @@ file_search <- function(path, pattern){
   # Returns a list with an indicator if it was a single occurrence, and path to all
   # ocurrences
   matching_files <- dir(path, pattern, full.names = TRUE)
-  single_file <- length(c1_files) > 0 & length(c1_files) < 2
+  n_files <- length(matching_files)
+  single_file <- n_files > 0 & n_files < 2
 
-  output <- list(single_file, path = matching_files)
+  output <- list(single_file = single_file, path = matching_files)
   return(output)
 }
+
+
+load_file <- function(path, col_def, delim = "|", filter_col = "PCIduration.ordinal") {
+  data <- read_delim(path, delim = "|", col_types = col_def) %>%
+    filter(!is.na(!!sym(filter_col)))
+}
+
+
+column_compare <- function(behav_name, df1, df2){
+
+  df1.extracted <- select(df1, behav_name) %>% pull()
+  df2.extracted <- select(df2, behav_name) %>% pull()
+
+  overlap_prop <- vector_overlap(df1.extracted, df2.extracted)
+
+  return(overlap_prop)
+}
+
+
+vector_overlap <- function(v1, v2) {
+
+  # Function that takes two vectors, compares them, and return a vector of overlaps
+  same <- (v1 == v2) | (is.na(v1) & is.na(v2))
+  same[is.na(same)] <- FALSE
+  return(same)
+}
+
+
+extract_vector <- function(df.in, behav_name){
+
+  df.extracted <- select(df.in, starts_with(behav_name) & ends_with("ordinal")) %>%
+    pull()
+
+  return(df.extracted)
+}
+
+find_differences <- function(rle_list, min_diff_length = 0){
+
+  if (length(rle_list$values) == 1){return()}
+
+  # Get the starting location of each block
+  rle_list$location <- c(1, cumsum(rle_list$lengths)[-length(rle_list$lengths)] + 1)
+
+  # Get the start location only for blocks where value is F (mismatch) and
+  # overlap is greater than specified above
+  diff_locations <- rle_list$location[!rle_list$values & rle_list$lengths >= min_diff_length]
+
+}
+
